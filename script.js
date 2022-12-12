ed10ceeddf7922ad9ec10596107bbc81c2f5f49d
ashan <indrajee79@gmail.com>
2022-12-12 20:47:38 +0530

    js commits

--- script.js
+++ script.js
@@ -16,3 +16,10 @@ const navToggle = document.querySelector(".nav-toggle");
 const linksContainer = document.querySelector(".links-container");
 const links = document.querySelector(".links");
 
+navToggle.addEventListener("click", function () {
+    //linksContainer.classList.toggle("show-links");
+})
+
+const linksHeight = links.getBoundingClientRect().height;
+const containerHeight = linksContainer.getBoundingClientRect().height;
+
