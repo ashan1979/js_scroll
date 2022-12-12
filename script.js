468e287173dfe4aaa4f61d9ea5367ddb126f8b60
ashan <indrajee79@gmail.com>
2022-12-12 20:52:39 +0530

    js commits

--- script.js
+++ script.js
@@ -18,8 +18,17 @@ const links = document.querySelector(".links");
 
 navToggle.addEventListener("click", function () {
     //linksContainer.classList.toggle("show-links");
-})
 
 const linksHeight = links.getBoundingClientRect().height;
 const containerHeight = linksContainer.getBoundingClientRect().height;
+
+if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+} else {
+    linksContainer.style.height = 0;
+}
+
+//console.log(linksContainer.getBoundingClientRect());
+
+});
 
